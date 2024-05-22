49a11ba2c1c1300c8110583626cd065e565243f2
laurahgdrn <62887619+laurahgdrn@users.noreply.github.com>
2024-05-22 12:41:14 +0200

    Update evaluateYOLO.py

--- YOLOcode/evaluateYOLO.py
+++ YOLOcode/evaluateYOLO.py
@@ -12,12 +12,6 @@ metrics.box.map50  # map50
 metrics.box.map75  # map75
 metrics.box.maps   # a list contains map50-95 of each category
 
-# Pose map50-95 
-# train101: 0.182
-# train7: 0.0621 
-# train3: 0.147
-# train: 0.237
-
 
 
 

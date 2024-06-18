2b3f6f8dcb2db44c5006e746c1a7b5cede9a2309
laurahgdrn <62887619+laurahgdrn@users.noreply.github.com>
2024-06-18 11:14:46 +0200

    Update YOLOMMACTION2.py

--- Preprocessing/YOLOMMACTION2.py
+++ Preprocessing/YOLOMMACTION2.py
@@ -1,3 +1,9 @@
+"""This code performs 2D skeleton based action recognition on unseen video data. 
+First, the pretrained YOLO pose estimation model is applied to each frame to extract the 
+poses of the macaques. The key points are collected over a number of frames (num_frames_per_action_rec)
+and then fed to the pretrained ST-GCN to perform action recognition."""
+
+
 import cv2
 import numpy as np
 from mmaction.apis import inference_skeleton, init_recognizer
@@ -14,7 +20,7 @@ def get_label(class_index):
 
 # Paths
 config_path = "C:/Users/hagedorn/mmaction2/configs/skeleton/stgcn/stgcn_8xb16-joint-motion-u100-80e_ntu60-xsub-keypoint-2d.py"
-checkpoint_path = "C:/Users/hagedorn/mmaction2/work_dirs/stgcn_8xb16-joint-motion-u100-80e_ntu60-xsub-keypoint-2d/best_acc_top1_epoch_200.pth"
+checkpoint_path = "C:/Users/hagedorn/mmaction2/work_dirs/stgcn_8xb16-joint-motion-u100-80e_ntu60-xsub-keypoint-2d/best_acc_top1_epoch_20.pth"
 output_video_path = "test_prediction.mp4"  # Path to save the output video
 
 # Function to detect poses using YOLO model
@@ -50,6 +56,7 @@ frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
 fourcc = cv2.VideoWriter_fourcc(*'mp4v')
 out = cv2.VideoWriter(output_video_path, fourcc, fps, (width, height))
 
+num_frames_per_action_rec = 100
 # Process video frames
 frame_number = 0
 while True:
@@ -60,30 +67,28 @@ while True:
     keypoints_over_frames = []
     scores_over_frames = []
     # Perform pose estimation every 30 frames
-    if frame_number % 30 == 0:
+    if frame_number % num_frames_per_action_rec == 0:
         # Detect poses using YOLO model
         keypoint_data, keypoint_score = detect_poses(frame)
         keypoints_over_frames.append(keypoint_data.tolist())
         scores_over_frames.append(keypoint_score.tolist())
-        print(keypoint_data)
-#         skeleton
 
-#         # Perform action recognition using skeleton data
-#         result = inference_skeleton(model, skeleton_data)
+        # Perform action recognition using skeleton data
+        result = inference_skeleton(model, skeleton_data)
 
-#         # Overlay action label on frame
-#         action_label = get_label(result['class'])
-#         cv2.putText(frame, action_label, (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+        # Overlay action label on frame
+        action_label = get_label(result['class'])
+        cv2.putText(frame, action_label, (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
 
-#     # Write frame to output video
-#     out.write(frame)
+    # Write frame to output video
+    out.write(frame)
 
-#     # Display frame
-#     cv2.imshow('Frame', frame)
-#     if cv2.waitKey(1) & 0xFF == ord('q'):
-#         break
+    # Display frame
+    cv2.imshow('Frame', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 
-# # Release video capture and writer
-# cap.release()
-# out.release()
-# cv2.destroyAllWindows()
+# Release video capture and writer
+cap.release()
+out.release()
+cv2.destroyAllWindows()

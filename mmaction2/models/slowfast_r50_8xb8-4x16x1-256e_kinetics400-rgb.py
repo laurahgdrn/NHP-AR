927a2be54573eb3eab89b6d147ba6c8cd8d2a7de
laurahgdrn <62887619+laurahgdrn@users.noreply.github.com>
2024-06-18 10:05:28 +0200

    Add files via upload

---
+++ mmaction2/models/slowfast_r50_8xb8-4x16x1-256e_kinetics400-rgb.py
@@ -0,0 +1,266 @@
+ann_file_test = '/Users/hagedorn/mmaction2/tools/data/macaques/val_labels.txt'
+ann_file_train = '/Users/hagedorn/mmaction2/tools/data/macaques/train_labels.txt'
+ann_file_val = '/Users/hagedorn/mmaction2/tools/data/macaques/val_labels.txt'
+data_root = '/Users/hagedorn/mmaction2/tools/data/macaques/train60'
+data_root_val = '/Users/hagedorn/mmaction2/tools/data/macaques/val60'
+dataset_type = 'VideoDataset'
+default_hooks = dict(
+    checkpoint=dict(interval=1, save_best='auto', type='CheckpointHook'),
+    logger=dict(ignore_last=False, interval=20, type='LoggerHook'),
+    param_scheduler=dict(type='ParamSchedulerHook'),
+    runtime_info=dict(type='RuntimeInfoHook'),
+    sampler_seed=dict(type='DistSamplerSeedHook'),
+    sync_buffers=dict(type='SyncBuffersHook'),
+    timer=dict(type='IterTimerHook'),
+    visualization=dict(
+        draw=False, interval=1, show=False, type='VisualizationHook'))
+default_scope = 'mmaction'
+env_cfg = dict(
+    cudnn_benchmark=False,
+    dist_cfg=dict(backend='nccl'),
+    mp_cfg=dict(mp_start_method='fork', opencv_num_threads=0))
+file_client_args = dict(io_backend='disk')
+launcher = 'none'
+load_from = None
+log_level = 'INFO'
+log_processor = dict(by_epoch=True, type='LogProcessor', window_size=20)
+model = dict(
+    backbone=dict(
+        channel_ratio=8,
+        fast_pathway=dict(
+            base_channels=8,
+            conv1_kernel=(
+                5,
+                7,
+                7,
+            ),
+            conv1_stride_t=1,
+            depth=50,
+            lateral=False,
+            norm_eval=False,
+            pool1_stride_t=1,
+            pretrained=None,
+            type='resnet3d'),
+        pretrained=None,
+        resample_rate=8,
+        slow_pathway=dict(
+            conv1_kernel=(
+                1,
+                7,
+                7,
+            ),
+            conv1_stride_t=1,
+            depth=50,
+            dilations=(
+                1,
+                1,
+                1,
+                1,
+            ),
+            inflate=(
+                0,
+                0,
+                1,
+                1,
+            ),
+            lateral=True,
+            norm_eval=False,
+            pool1_stride_t=1,
+            pretrained=None,
+            type='resnet3d'),
+        speed_ratio=8,
+        type='ResNet3dSlowFast'),
+    cls_head=dict(
+        average_clips='prob',
+        dropout_ratio=0.5,
+        in_channels=2304,
+        num_classes=2,
+        spatial_type='avg',
+        type='SlowFastHead'),
+    data_preprocessor=dict(
+        format_shape='NCTHW',
+        mean=[
+            123.675,
+            116.28,
+            103.53,
+        ],
+        std=[
+            58.395,
+            57.12,
+            57.375,
+        ],
+        type='ActionDataPreprocessor'),
+    type='Recognizer3D')
+optim_wrapper = dict(
+    clip_grad=dict(max_norm=40, norm_type=2),
+    optimizer=dict(lr=0.1, momentum=0.9, type='SGD', weight_decay=0.0001))
+param_scheduler = [
+    dict(
+        begin=0,
+        by_epoch=True,
+        convert_to_iter_based=True,
+        end=3,
+        start_factor=0.1,
+        type='LinearLR'),
+    dict(
+        T_max=20,
+        begin=0,
+        by_epoch=True,
+        end=20,
+        eta_min=0,
+        type='CosineAnnealingLR'),
+]
+randomness = dict(deterministic=False, diff_rank_seed=False, seed=None)
+resume = False
+test_cfg = dict(type='TestLoop')
+test_dataloader = dict(
+    batch_size=1,
+    dataset=dict(
+        ann_file='/Users/hagedorn/mmaction2/tools/data/macaques/val_labels.txt',
+        data_prefix=dict(
+            video='/Users/hagedorn/mmaction2/tools/data/macaques/val60'),
+        pipeline=[
+            dict(io_backend='disk', type='DecordInit'),
+            dict(
+                clip_len=32, # frames of each sampled output clip 
+                frame_interval=16, # temporal interval of adjacent sampled frames
+                num_clips=10,
+                test_mode=True,
+                type='SampleFrames'),
+            dict(type='DecordDecode'),
+            dict(scale=(
+                -1,
+                256,
+            ), type='Resize'),
+            dict(crop_size=256, type='ThreeCrop'),
+            dict(input_format='NCTHW', type='FormatShape'),
+            dict(type='PackActionInputs'),
+        ],
+        test_mode=True,
+        type='VideoDataset'),
+    num_workers=8,
+    persistent_workers=True,
+    sampler=dict(shuffle=False, type='DefaultSampler'))
+test_evaluator = dict(type='AccMetric')
+test_pipeline = [
+    dict(io_backend='disk', type='DecordInit'),
+    dict(
+        clip_len=32,
+        frame_interval=2,
+        num_clips=10,
+        test_mode=True,
+        type='SampleFrames'),
+    dict(type='DecordDecode'),
+    dict(scale=(
+        -1,
+        256,
+    ), type='Resize'),
+    dict(crop_size=256, type='ThreeCrop'),
+    dict(input_format='NCTHW', type='FormatShape'),
+    dict(type='PackActionInputs'),
+]
+train_cfg = dict(
+    max_epochs=20, type='EpochBasedTrainLoop', val_begin=1, val_interval=1)
+train_dataloader = dict(
+    batch_size=8,
+    dataset=dict(
+        ann_file='/Users/hagedorn/mmaction2/tools/data/macaques/train_labels.txt',
+        data_prefix=dict(
+            video='/Users/hagedorn/mmaction2/tools/data/macaques/train60'),
+        pipeline=[
+            dict(io_backend='disk', type='DecordInit'),
+            dict(
+                clip_len=32,
+                frame_interval=2,
+                num_clips=1,
+                type='SampleFrames'),
+            dict(type='DecordDecode'),
+            dict(scale=(
+                -1,
+                256,
+            ), type='Resize'),
+            dict(type='RandomResizedCrop'),
+            dict(keep_ratio=False, scale=(
+                224,
+                224,
+            ), type='Resize'),
+            dict(flip_ratio=0.5, type='Flip'),
+            dict(input_format='NCTHW', type='FormatShape'),
+            dict(type='PackActionInputs'),
+        ],
+        type='VideoDataset'),
+    num_workers=8,
+    persistent_workers=True,
+    sampler=dict(shuffle=True, type='DefaultSampler'))
+train_pipeline = [
+    dict(io_backend='disk', type='DecordInit'),
+    dict(clip_len=32, frame_interval=2, num_clips=1, type='SampleFrames'),
+    dict(type='DecordDecode'),
+    dict(scale=(
+        -1,
+        256,
+    ), type='Resize'),
+    dict(type='RandomResizedCrop'),
+    dict(keep_ratio=False, scale=(
+        224,
+        224,
+    ), type='Resize'),
+    dict(flip_ratio=0.5, type='Flip'),
+    dict(input_format='NCTHW', type='FormatShape'),
+    dict(type='PackActionInputs'),
+]
+val_cfg = dict(type='ValLoop')
+val_dataloader = dict(
+    batch_size=8,
+    dataset=dict(
+        ann_file='/Users/hagedorn/mmaction2/tools/data/macaques/val_labels.txt',
+        data_prefix=dict(
+            video='/Users/hagedorn/mmaction2/tools/data/macaques/val60'),
+        pipeline=[
+            dict(io_backend='disk', type='DecordInit'),
+            dict(
+                clip_len=32,
+                frame_interval=2,
+                num_clips=1,
+                test_mode=True,
+                type='SampleFrames'),
+            dict(type='DecordDecode'),
+            dict(scale=(
+                -1,
+                256,
+            ), type='Resize'),
+            dict(crop_size=224, type='CenterCrop'),
+            dict(input_format='NCTHW', type='FormatShape'),
+            dict(type='PackActionInputs'),
+        ],
+        test_mode=True,
+        type='VideoDataset'),
+    num_workers=8,
+    persistent_workers=True,
+    sampler=dict(shuffle=False, type='DefaultSampler'))
+val_evaluator = dict(type='AccMetric')
+val_pipeline = [
+    dict(io_backend='disk', type='DecordInit'),
+    dict(
+        clip_len=32,
+        frame_interval=2,
+        num_clips=1,
+        test_mode=True,
+        type='SampleFrames'),
+    dict(type='DecordDecode'),
+    dict(scale=(
+        -1,
+        256,
+    ), type='Resize'),
+    dict(crop_size=224, type='CenterCrop'),
+    dict(input_format='NCTHW', type='FormatShape'),
+    dict(type='PackActionInputs'),
+]
+vis_backends = [
+    dict(type='WandbVisBackend'),
+]
+visualizer = dict(
+    type='ActionVisualizer', vis_backends=[
+        dict(type='WandbVisBackend'),
+    ])
+work_dir = './work_dirs/slowfast_r50_8xb8-4x16x1-256e_kinetics400-rgb'
---
+++ mmaction2/models/stgcn_8xb16-joint-u100-80e_ntu60-xsub-keypoint-3d.py
@@ -0,0 +1,166 @@
+ann_file = 'C:/Users/hagedorn/Desktop/data/macaques_skeleton_3d.pkl'
+auto_scale_lr = dict(base_batch_size=128, enable=False)
+dataset_type = 'PoseDataset'
+default_hooks = dict(
+    checkpoint=dict(interval=1, save_best='auto', type='CheckpointHook'),
+    logger=dict(ignore_last=False, interval=100, type='LoggerHook'),
+    param_scheduler=dict(type='ParamSchedulerHook'),
+    runtime_info=dict(type='RuntimeInfoHook'),
+    sampler_seed=dict(type='DistSamplerSeedHook'),
+    sync_buffers=dict(type='SyncBuffersHook'),
+    timer=dict(type='IterTimerHook'),
+    visualization=dict(
+        draw=False, interval=1, show=False, type='VisualizationHook'))
+default_scope = 'mmaction'
+env_cfg = dict(
+    cudnn_benchmark=False,
+    dist_cfg=dict(backend='nccl'),
+    mp_cfg=dict(mp_start_method='fork', opencv_num_threads=0))
+launcher = 'none'
+load_from = 'C:/Users/hagedorn/mmaction2/work_dirs/stgcn_8xb16-joint-u100-80e_ntu60-xsub-keypoint-3d/best_acc_top1_epoch_20.pth'
+log_level = 'INFO'
+log_processor = dict(by_epoch=True, type='LogProcessor', window_size=20)
+model = dict(
+    backbone=dict(
+        graph_cfg=dict(layout='coco', mode='stgcn_spatial'), type='STGCN'),
+    cls_head=dict(in_channels=256, num_classes=2, type='GCNHead'),
+    type='RecognizerGCN')
+optim_wrapper = dict(
+    optimizer=dict(
+        lr=0.1, momentum=0.9, nesterov=True, type='SGD', weight_decay=0.0005))
+param_scheduler = [
+    dict(
+        T_max=16,
+        by_epoch=True,
+        convert_to_iter_based=True,
+        eta_min=0,
+        type='CosineAnnealingLR'),
+]
+randomness = dict(deterministic=False, diff_rank_seed=False, seed=None)
+resume = False
+test_cfg = dict(type='TestLoop')
+test_dataloader = dict(
+    batch_size=1,
+    dataset=dict(
+        ann_file='C:/Users/hagedorn/Desktop/data/macaques_skeleton_3d.pkl',
+        pipeline=[
+            dict(type='PreNormalize3D'),
+            dict(dataset='coco', feats=[
+                'j',
+            ], type='GenSkeFeat'),
+            dict(
+                clip_len=100,
+                num_clips=10,
+                test_mode=True,
+                type='UniformSampleFrames'),
+            dict(type='PoseDecode'),
+            dict(num_person=2, type='FormatGCNInput'),
+            dict(type='PackActionInputs'),
+        ],
+        split='xsub_val',
+        test_mode=True,
+        type='PoseDataset'),
+    num_workers=2,
+    persistent_workers=True,
+    sampler=dict(shuffle=False, type='DefaultSampler'))
+test_evaluator = [
+    dict(type='AccMetric'),
+]
+test_pipeline = [
+    dict(type='PreNormalize3D'),
+    dict(dataset='coco', feats=[
+        'j',
+    ], type='GenSkeFeat'),
+    dict(
+        clip_len=100, num_clips=10, test_mode=True,
+        type='UniformSampleFrames'),
+    dict(type='PoseDecode'),
+    dict(num_person=2, type='FormatGCNInput'),
+    dict(type='PackActionInputs'),
+]
+train_cfg = dict(
+    max_epochs=20, type='EpochBasedTrainLoop', val_begin=1, val_interval=5)
+train_dataloader = dict(
+    batch_size=16,
+    dataset=dict(
+        dataset=dict(
+            ann_file='C:/Users/hagedorn/Desktop/data/macaques_skeleton_3d.pkl',
+            pipeline=[
+                dict(type='PreNormalize3D'),
+                dict(dataset='coco', feats=[
+                    'j',
+                ], type='GenSkeFeat'),
+                dict(clip_len=100, type='UniformSampleFrames'),
+                dict(type='PoseDecode'),
+                dict(num_person=2, type='FormatGCNInput'),
+                dict(type='PackActionInputs'),
+            ],
+            split='xsub_train',
+            type='PoseDataset'),
+        times=5,
+        type='RepeatDataset'),
+    num_workers=2,
+    persistent_workers=True,
+    sampler=dict(shuffle=True, type='DefaultSampler'))
+train_pipeline = [
+    dict(type='PreNormalize3D'),
+    dict(dataset='coco', feats=[
+        'j',
+    ], type='GenSkeFeat'),
+    dict(clip_len=100, type='UniformSampleFrames'),
+    dict(type='PoseDecode'),
+    dict(num_person=2, type='FormatGCNInput'),
+    dict(type='PackActionInputs'),
+]
+val_cfg = dict(type='ValLoop')
+val_dataloader = dict(
+    batch_size=16,
+    dataset=dict(
+        ann_file='C:/Users/hagedorn/Desktop/data/macaques_skeleton_3d.pkl',
+        pipeline=[
+            dict(type='PreNormalize3D'),
+            dict(dataset='coco', feats=[
+                'j',
+            ], type='GenSkeFeat'),
+            dict(
+                clip_len=100,
+                num_clips=1,
+                test_mode=True,
+                type='UniformSampleFrames'),
+            dict(type='PoseDecode'),
+            dict(num_person=2, type='FormatGCNInput'),
+            dict(type='PackActionInputs'),
+        ],
+        split='xsub_val',
+        test_mode=True,
+        type='PoseDataset'),
+    num_workers=2,
+    persistent_workers=True,
+    sampler=dict(shuffle=False, type='DefaultSampler'))
+val_evaluator = [
+    dict(type='AccMetric'),
+]
+val_pipeline = [
+    dict(type='PreNormalize3D'),
+    dict(dataset='coco', feats=[
+        'j',
+    ], type='GenSkeFeat'),
+    dict(
+        clip_len=100, num_clips=1, test_mode=True, type='UniformSampleFrames'),
+    dict(type='PoseDecode'),
+    dict(num_person=2, type='FormatGCNInput'),
+    dict(type='PackActionInputs'),
+]
+vis_backends = [
+    dict(type='LocalVisBackend'),
+    dict(type='TensorboardVisBackend'),
+    dict(type='WandbVisBackend'),
+]
+visualizer = dict(
+    type='ActionVisualizer',
+    vis_backends=[
+        dict(type='LocalVisBackend'),
+        dict(type='TensorboardVisBackend'),
+        dict(type='WandbVisBackend'),
+    ])
+work_dir = './work_dirs\\stgcn_8xb16-joint-u100-80e_ntu60-xsub-keypoint-3d'
